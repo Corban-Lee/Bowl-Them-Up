2c6e001af56465b85d2355663ef4fe77e96a06b8
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-11-24 15:12:46 +0000

    Added AlleyLanes Model

---
+++ BowlThemUp/Models/AlleyLanes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlThemUp.Models
+{
+    public class AlleyLanes
+    {
+        public int LaneNo { get; set; }
+        public string Status { get; set; }
+        public string Title { get; set; }
+    }
+}
