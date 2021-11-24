4fc80b7587f2e10163e20e8748d86a3e6412ed4f
Matthew Palmer <833497@student.derby-college.ac.uk>
2021-11-24 14:45:27 +0000

    Player Details (DB)

---
+++ BowlThemUp/Models/PlayerDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlThemUp.Models
+{
+    public class PlayerDetails
+    {
+        public int PlayerID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int ShoeSize { get; set; }
+        public int BallSize { get; set; }
+
+        public virtual ICollection<BookingDetails>BookingDetails { get; set; }
+    }
+}
