2fd2b2f4c46ce219a9f48d3d65314ff70c1fb09b
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-12-01 10:15:07 +0000

    uploaded django version of project

---
+++ website/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "views/index.html")
+
+
+def test(request):
+    return render(request, "views/test.html")
