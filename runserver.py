2c2cdcc2be1a30712bd75799d597f4f41eea5bda
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-12-06 09:47:29 +0000

    improved comments

--- runserver.py
+++ runserver.py
@@ -1,7 +1,11 @@
 
 #
-#  Run this file to start the server
-#  DO NOT RUN THIS FILE IN DEBUG MODE (vscode)
+#  Run this file to start the server.
+#  To stop the server use ^C (ctrl + c) in the terminal at the /
+#  bottom of your screen.
+#
+#  DO NOT RUN THIS FILE IN DEBUG MODE (vscode).
+#  DO NOT RUN THIS FILE TWICE WITHOUT STOPPING THE SERVER.
 #
 
 from subprocess import call
