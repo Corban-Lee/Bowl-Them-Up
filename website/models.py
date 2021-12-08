16f0e022e8cc1fa7d681acbaf59076c6be6b285d
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-12-08 09:56:31 +0000

    Added comments and status attribute to alleylane entity

--- website/models.py
+++ website/models.py
@@ -3,6 +3,7 @@ from django.db.models.base import Model
 from django.db.models.fields.related import ForeignKey
 
 class Customer(models.Model):
+    """Customers. These are the people that use the website and book games with the bowling alley"""
     UserID = models.AutoField(primary_key=True)
     Username = models.CharField(max_length=64, unique=True)
     FirstName = models.CharField(max_length=64)
@@ -12,6 +13,7 @@ class Customer(models.Model):
     Password = models.CharField(max_length=64)
 
 class Player(models.Model):
+    """These are the people that show up to play a game at the bowling alley"""
     PlayerID = models.AutoField(primary_key=True)
     FirstName = models.CharField(max_length=64)
     LastName = models.CharField(max_length=64)
@@ -19,11 +21,13 @@ class Player(models.Model):
     BallSize = models.IntegerField()
 
 class AlleyLane(models.Model):
+    """This entity represents a lane in the bowling alley"""
     AlleyLaneID = models.AutoField(primary_key=True)
     Title = models.CharField(max_length=32)
-    models.CharField(max_length=32)
+    Status = models.CharField(max_length=32, default="closed")
 
 class Bookings(models.Model):
+    """Similar to an online order. This entity contains information regarding a customers arangement to use a bowling alley lane."""
     BookingsID = models.AutoField(primary_key=True)
     CustomerID = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
     PlayerID = models.ForeignKey(to=Player, on_delete=models.CASCADE)
