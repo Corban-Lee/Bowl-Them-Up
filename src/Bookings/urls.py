e944d48de55c74cdc8f1035686e4f2499457bf6c
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-12-15 14:46:20 +0000

    updated view bookings

--- src/Bookings/urls.py
+++ src/Bookings/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_bookings, name="all_bookings"),
-    path('create/', views.create, name="create_booking"),
-    path('edit/', views.edit, name="edit_booking"),
-    path('delete/', views.delete, name="delete_booking"),
+    #     path          function                    call-name
+    path('',         views.view_bookings, name="view_bookings"      ),
+    path('<int:id>', views.view_bookings, name="view_user_bookings" ),
+    path('create/',  views.create,        name="create_booking"     ),
+    path('edit/',    views.edit,          name="edit_booking"       ),
+    path('delete/',  views.delete,        name="delete_booking"     ),
 ]
--- src/Bookings/views.py
+++ src/Bookings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Booking
+from .forms import NewBookingForm
 
 # Create your views here.
 
@@ -9,18 +10,28 @@ def view_bookings( request, **kwargs  ):
     """
     view bookings page when the user is logged in
     """
-    if '_id' in kwargs and request.user.is_authenticated:
-        data = { 'booking_entries': Booking.object.filter( CustomerID = _id ) }
+    user = request.user
+
+    # if 'id' in kwargs:
+    #     _id = kwargs.pop( 'id' )
+
+    #     match user.is_authenticated:
+    #         case 1:
+    #             pass
+
+    if 'id' in kwargs and user.is_authenticated:
+        _id = kwargs.pop( 'id' )
+        if _id != user.UserID:
+            raise Http404( 'You dont have access to this page. I should use a different code but I dont know how; that is why this is a 404' )
+
+        data = { 'booking_entries': Booking.objects.filter( CustomerID = _id ) }
         return render( request, 'viewbookings.html', data )
 
-    elif '_id' in kwargs and not request.user.is_authenticated:
+    elif 'id' in kwargs and not user.is_authenticated:
         raise Http404( 'You dont have access to this page. I should use a different code but I dont know how; that is why this is a 404' )
 
-    elif 'id' not in kwargs and request.user.is_authenticated:
-        return redirect('view_user_bookings')
-        #
-        #  redirect url bookings/<int:id>/ if they 
-        #
+    elif 'id' not in kwargs and user.is_authenticated:
+        return redirect('view_user_bookings', user.UserID)
 
     else:
         return render( request, 'viewbookings.html', {} )
@@ -28,7 +39,18 @@ def view_bookings( request, **kwargs  ):
 
 
 def create(request):
-    return render(request, 'createbooking.html', {})
+    """
+    create new bookings
+    """
+    if request.method == 'POST':
+        form = NewBookingForm(request.POST)
+        if form.is_valid():
+            return redirect('view_bookings')
+
+    else:
+        form = NewBookingForm()
+
+    return render(request, 'bookingform.html', {'createform': form})
 
 
 def edit(request):
