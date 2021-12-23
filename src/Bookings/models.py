0b2e6649961cfc807b6d7908ad5b0e9e23c666fe
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-12-23 03:51:11 +0000

    Finalised BookingModel & LaneModel

--- src/Bookings/models.py
+++ src/Bookings/models.py
@@ -1,28 +1,26 @@
 from django.db import models
+from django.forms.models import modelformset_factory
 
 # Create your models here.
 
 from Accounts.models import Account
 
 
-class AlleyLane(models.Model):
-    AlleyLaneID = models.AutoField(primary_key=True)
+class LaneModel(models.Model):
+    """Model for the alley lanes in the bowling alley"""
+
+    Id = models.AutoField(primary_key=True)
     Available   = models.BooleanField(null=False, default=False)
 
 
-class Player(models.Model):
-    PlayerID   = models.AutoField(primary_key=True)
-    ShoeSize   = models.IntegerField(default=False)
-    BallSize   = models.IntegerField(default=False)
+class BookingModel(models.Model):
+    """Model for the customer created bookings"""
 
-
-class Booking(models.Model):
-    BookingID   = models.AutoField(primary_key=True)
-    CustomerID  = models.ForeignKey(to=Account, on_delete=models.CASCADE)
-    AlleyLaneID = models.ForeignKey(to=AlleyLane, on_delete=models.CASCADE)
-    Players     = models.ManyToManyField(Player)
-    NumberOfPlayers = models.IntegerField()
-    DateTime    = models.DateTimeField(null=False)
+    Id              = models.AutoField(primary_key=True)
+    Customer        = models.ForeignKey(to=Account, on_delete=models.CASCADE)
+    Lane            = models.ForeignKey(to=LaneModel, on_delete=models.CASCADE)
+    DateTime        = models.DateTimeField(null=False)
+    NumberOfPlayers = models.IntegerField(null=False)
 
 
 
