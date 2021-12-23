10330e11ffc0a06c530d659725f78ab39cf4f8ee
Corban-Lee <77944149+XordK@users.noreply.github.com>
2021-12-23 03:52:50 +0000

    added comments

--- src/Bookings/urls.py
+++ src/Bookings/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     #     path          function                    call-name
-    path('',         views.view_bookings, name="view_bookings"      ),
-    path('<int:id>', views.view_bookings, name="view_user_bookings" ),
-    path('create/',  views.create,        name="create_booking"     ),
-    path('edit/',    views.edit,          name="edit_booking"       ),
-    path('delete/',  views.delete,        name="delete_booking"     ),
+    path('',         views.view_bookings, name="view_bookings"      ),   # show the booking page to the user
+    path('<int:id>', views.view_bookings, name="view_user_bookings" ),   # show the user their bookings (if logged in)
+    path('create/',  views.create,        name="create_booking"     ),   # create a booking
+    path('edit/',    views.edit,          name="edit_booking"       ),   # edit a booking
+    path('delete/',  views.delete,        name="delete_booking"     ),   # delete a booking
 ]
